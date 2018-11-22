434489198b0d12b9f721542ab6b9f3ac3746684c
chris <krueger@creative-network.biz>
2018-11-22 12:50:06 +0100

    Bugfix the install.sh
    Needed to add vimrcPRE for install the vim-plug without error.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm ~/.vimrc ; rm -rf ~/.vim
 # mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vimrcPRE ~/.vimrc
 vim -c :PlugInstall
 cp vimrc ~/.vimrc
 # cp jsbeautifyrc ~/.jsbeautifyrc
