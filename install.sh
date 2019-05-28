a0abb6be2951a546cabf25059d646381c360991c
chris <krueger@creative-network.biz>
2019-05-28 16:09:02 +0200

    Change Installter to autoquit vim after install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ rm ~/.vimrc ; rm -rf ~/.vim
 # mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrcPRE ~/.vimrc
-vim -c :PlugInstall
+vim +PlugInstall +PlugUpgrade +PlugUpdate +qa!
 cp vimrc ~/.vimrc
 cp jsbeautifyrc ~/.jsbeautifyrc
 
